04f4195c4dd12d2ba10b1e109f1fc2b628dab840
Ilham shadiq <if23.muhammadshadiq@mhs.ubpkarawang.ac.id>
2024-11-29 00:18:03 +0700

    Add files via upload

--- backend/login.php
+++ backend/login.php
@@ -1,38 +1,13 @@
-<?php
+<?php 
+    if(isset($_POST['email']) || isset($_POST['password']) ) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-require __DIR__ . '/../config/db.php';
-
-if (isset($_POST['submit'])) {
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    // Query untuk mencari pengguna berdasarkan email
-    $user = mysqli_query($db_connect, "SELECT * FROM users WHERE email = '$email'");
-
-    if (mysqli_num_rows($user) > 0) {
-        $data = mysqli_fetch_assoc($user);
-        
-        // Verifikasi password
-        if (password_verify($password, $data['password'])) {
-            // Cek role pengguna
-            if ($data['role'] === 'admin') {
-                // Redirect ke halaman admin
-                header('Location: ../profile.php');
-                exit();
-            } else {
-                // Tampilkan pesan untuk user biasa
-                echo "Selamat datang, " . htmlspecialchars($data['name']);
-                exit(); // Hentikan eksekusi setelah pesan ditampilkan
-            }
+        if($email == 'admin@admin.com' && $password == 'admin') {
+            header('Location: ./../dashboard.php');
         } else {
-            echo '<div class="alert alert-danger">Password salah!</div>';
-            exit();
+            echo "Email atau password salah";
         }
-
-    } else {
-        echo '<div class="alert alert-danger">Email atau password salah!</div>';
-        exit();
     }
-}
+
 ?>
